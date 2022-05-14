be537145813b7c3f7d064262016222050798e592
Eric Patterson <ericpattersonwork@hotmail.com>
2022-05-13 20:25:31 -0600

    added search filter

--- src/components/Home/PostData.js
+++ src/components/Home/PostData.js
@@ -3,8 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchPassengers } from "../../store/actions";
 import { getPassengers } from "../../store/selectors";
 import Button from "../UI/Button";
+import { useState } from "react";
 
 const PostData = (props) => {
+  // state for rearch
+  const [searchTerm, setSearchTerm] = useState("");
+
   const dispatch = useDispatch();
   const passengers = useSelector(getPassengers);
 
@@ -12,28 +16,38 @@ const PostData = (props) => {
     dispatch(fetchPassengers());
   }, [dispatch]);
 
-  // console.log(passengers, "total");
+  console.log(passengers, "total");
 
   return (
     <div>
       <h1>Posts</h1>
       <input
         type="text"
-        placeholder="Search by name"
+        placeholder="Search by ID"
         onChange={(e) => {
-          console.log(e.target.value);
+          setSearchTerm(e.target.value);
         }}
       />
-      {passengers.map((post) => (
-        <div key={post.id}>
-          <h2>Title: {post.title}</h2>
-          <p>Post Number: {post.id}</p>
-          <p>Description: {post.body}</p>
-          <Button onClick={props.onShowCart}>Add</Button>
-          <Button>Edit</Button>
-          <Button>Delete</Button>
-        </div>
-      ))}
+      {/* filters the api first, then checks for number{id} and maps over the filtered data */}
+      {passengers
+        .filter((val) => {
+          if (searchTerm === "") {
+            return val;
+          } else if (val.id === parseInt(searchTerm, 10)) {
+            console.log(searchTerm);
+            return val;
+          }
+        })
+        .map((post) => (
+          <div key={post.id}>
+            <h2>Title: {post.title}</h2>
+            <p>Post Number: {post.id}</p>
+            <p>Description: {post.body}</p>
+            <Button onClick={props.onShowCart}>Add</Button>
+            <Button>Edit</Button>
+            <Button>Delete</Button>
+          </div>
+        ))}
     </div>
   );
 };
