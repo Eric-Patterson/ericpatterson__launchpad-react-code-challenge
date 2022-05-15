c590ca9cbde65f9b4706cf8005a0bdadd4960829
Eric Patterson <ericpattersonwork@hotmail.com>
2022-05-14 20:39:08 -0600

    added universities and country api middleware and pulled to page

--- src/components/Universities/Universities.js
+++ src/components/Universities/Universities.js
@@ -1,7 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUniversityData } from "../../store/actions";
+import { getUniversityDataSelector } from "../../store/selectors";
+
+import { getCountryData } from "../../store/actions";
+import { getCountryDataSelector } from "../../store/selectors";
+
 function Universities() {
+  const dispatch = useDispatch();
+
+  const universities = useSelector(getUniversityDataSelector);
+
+  const countries = useSelector(getCountryDataSelector);
+
+  useEffect(() => {
+    dispatch(getCountryData());
+    dispatch(getUniversityData());
+  }, [dispatch]);
+
+  // useEffect(() => {
+  //   dispatch(getUniversityData());
+  // }, [dispatch]);
+
   return (
     <div>
+      <form>
+        <select>
+          <option value="">Select a Country</option>
+          {countries.map((country, index) => (
+            <option key={index} value={country.id}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </form>
       <h1>Universities</h1>
+      {/* map over universities */}
+      {universities.map((university, index) => (
+        <div key={index}>
+          <h2>Country: {university.country}</h2>
+          <h2>Name: {university.name}</h2>
+          <h2>Website: {university.web_pages[0]}</h2>
+        </div>
+      ))}
     </div>
   );
 }
